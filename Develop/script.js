f49203250d02289135a44e0ca2c2670371eda042
Alex Harrington <spirithoula2@gmail.com>
2021-04-03 16:45:27 -0500

    password length changes

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//click the button to get prompt box
+function generatePassword() {
+  //prompts user to enter a password length
+  var passwordLength = prompt("Enter a password length between 8-128 characters");
+  // if (isNaN(passwordLength)){
+  //   alert("enter a number")
+  // }
+  // if (passwordLength < 8) {
+  //   alert("password too short")
+  // } else if (passwordLength > 128) {
+  //   alert("password too long")
+  // }
+  var useLowercase = window.confirm("Do you want lowercase in your password?");
+}
